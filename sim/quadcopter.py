24ddf9d244f139d71a518148017810b8e0648e21
pravinas <pravinas@mit.edu>
2014-09-21 12:07:50 -0400

    More fleshing out of the skeletons and pygame.
    Nonfunctional step() functions added to the classes.

--- sim/quadcopter.py
+++ sim/quadcopter.py
@@ -1,2 +1,8 @@
 class Quadcopter:
-	pass
+	def __init__(self, x = 0, y = 0):
+		self.x = x
+		self.y = y
+		#... add anything you think of
+
+	def step(self): # you can add whatever args you need
+		pass
