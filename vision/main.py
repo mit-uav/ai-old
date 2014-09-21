1e4786287061d06d8dfa3cd6d894816b866dac58
Marcus Boorstin <mindstormsguy@verizon.net>
2014-09-21 12:06:11 -0400

    Vision: Bit better Hough

--- vision/main.py
+++ vision/main.py
@@ -2,18 +2,19 @@ import numpy as np
 import cv2
 import cv2.cv as cv
 
-img = cv2.imread('test2.jpg', cv2.CV_LOAD_IMAGE_GRAYSCALE)
-cimg = cv2.medianBlur(img,5)
-#cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+img = cv2.imread('test2.jpg', cv2.CV_LOAD_IMAGE_COLOR)
 
-circles = cv2.HoughCircles(img,cv.CV_HOUGH_GRADIENT,1,20, param1=50,param2=68, minRadius=5,maxRadius=350)
+cimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cimg = cv2.medianBlur(cimg,5)
+
+circles = cv2.HoughCircles(cimg,cv.CV_HOUGH_GRADIENT,1,20, param1=50,param2=59, minRadius=5,maxRadius=330)
 circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
+for c in circles[0,:]:
     # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    cv2.circle(img,(c[0],c[1]),c[2],(0,255,0),2)
     # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    cv2.circle(img,(c[0],c[1]),2,(0,0,255),3)
 
-cv2.imshow('detected circles',cimg)
+cv2.imshow('Detected Circles',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
