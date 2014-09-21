04cd9c0ce4a65402f04bdb1bf462c08ea7f3926d
Marcus Boorstin <mindstormsguy@verizon.net>
2014-09-21 11:22:19 -0400

    Vision: Hugh Circle Transform Test

--- vision/main.py
+++ vision/main.py
@@ -1,8 +1,19 @@
 import numpy as np
 import cv2
+import cv2.cv as cv
 
-img = cv2.imread('test.jpg', cv2.CV_LOAD_IMAGE_COLOR)
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.imshow('image', img)
+img = cv2.imread('test2.jpg', cv2.CV_LOAD_IMAGE_GRAYSCALE)
+cimg = cv2.medianBlur(img,5)
+#cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img,cv.CV_HOUGH_GRADIENT,1,20, param1=50,param2=68, minRadius=5,maxRadius=350)
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imshow('detected circles',cimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
